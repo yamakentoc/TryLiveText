7a92b3e962a56bddd054d65dfdf7d625a74cfdca
yamakentoc <yamakentoc@gmail.com>
2022-08-08 14:56:05 +0900

    first commit

---
+++ TryLiveText/TryLiveTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryLiveTextApp.swift
+//  TryLiveText
+//
+//  Created by 山口賢登 on 2022/08/07.
+//
+
+import SwiftUI
+
+@main
+struct TryLiveTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
